60adfe24f56b974c5d7739b4b2594f8ea3185d94
煜婷王 <yutingwang@r-55-105-25-172.comp.nus.edu.sg>
2019-10-29 20:03:17 +0800

    login data retrieve (cont')

--- routes/navbar.js
+++ routes/navbar.js
@@ -7,8 +7,20 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
 
+var getUserIdentification = 'SELECT count(*) from users where ';
+
 router.get('/', function(req, res, next) {
   res.render('navbar', { title: 'Navbar' });
 });
 
+router.post('/', function(req, res, next) {
+	var email = req.body.email;
+	var password = req.body.pw;
+	
+	sql_query += 'email = ' + email + ' and password = ' + password;
+	
+	pool.query(insert_query, (err, data) => {
+		res.redirect('/select')
+	});
+});
 module.exports = router;
