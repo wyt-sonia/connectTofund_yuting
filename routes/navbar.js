875b2a9afac4bca54ad7b6d2aa27b080debb34b9
煜婷王 <yutingwang@wyt.local>
2019-10-29 23:38:28 +0800

    login function

--- routes/navbar.js
+++ routes/navbar.js
@@ -7,31 +7,31 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
 
-var getUserIdentification = 'SELECT count(*) from users where ';
-
 router.get('/', function(req, res, next) {
   res.render('navbar', { title: 'Navbar' });
 });
 
 router.post('/', function(req, res, next) {
-	var email = req.body.email;
-	var password = req.body.pw;
+	var usernameEmail = req.body.usernameEmail;
+  var password = req.body.pw;
+  var login_query = '';
+
+  console.log(usernameEmail + " " + password);
 	
-	sql_query += 'email = ' + email + ' and password = ' + password;
+  login_query = 'SELECT count(*) as result from users where (email = \'' + usernameEmail + '\' or username = \'' 
+                + usernameEmail + '\') and password = \'' + password + '\'';
+  
+  console.log(login_query);
 	
-	pool.query(insert_query, (err, data) => {
-		res.redirect('/select')
+	pool.query(login_query, (err, data) => {
+		if(data != null && data.rows[0].result == 1) {
+      res.redirect('/');
+      console.log("Login succeed.");
+    } else {
+      res.redirect('/');
+      console.log("Login failed.");
+    }
 	});
 });
 
-router.post('/', function(req, res, next) {
-	var email = req.body.email;
-	var password = req.body.pw;
-	
-	sql_query += 'email = ' + email + ' and password = ' + password;
-	
-	pool.query(insert_query, (err, data) => {
-		res.redirect('/select')
-	});
-});
 module.exports = router;
