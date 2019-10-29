3f84f00a02062bddb92a6e74c1000c07a207f066
Zhu Weijie <zhuweijie11@gmail.com>
2019-10-29 17:59:30 +0800

    update connect to database

--- routes/navbar.js
+++ routes/navbar.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg')
+
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
 router.get('/', function(req, res, next) {
   res.render('navbar', { title: 'Navbar' });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg')
+
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
