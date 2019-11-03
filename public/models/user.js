d423767e9d58d262c6ccefffc312083898998f34
煜婷王 <yutingwang@wyt.local>
2019-11-03 14:24:03 +0800

    view_project UI design and filter by name or category

---
+++ public/models/user.js
@@ -0,0 +1,8 @@
+class user {
+    constructor(email, phoneNumber, username, location) {
+      this.email = email
+      this.phoneNumber = phoneNumber
+      this.username = username
+      this.location = location
+    }
+  }
