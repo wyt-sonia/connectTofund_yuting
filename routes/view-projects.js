17dae232bbce140329ca0a592f1cd706759eb4c5
煜婷王 <yutingwang@wyt.local>
2019-11-03 15:52:59 +0800

    like follow databind

--- routes/view-projects.js
+++ routes/view-projects.js
@@ -11,11 +11,14 @@ const pool = new Pool({
 var cateTemp = null;
 var projTemp = null;
 var categories_query = 'SELECT * FROM categories';
-var project_query = 'SELECT DISTINCT ON ("projectStartDate","projectName") * FROM projects '
+var project_query = 'SELECT DISTINCT ON ("projectStartDate","projectName") p.*, '
+                  +'(SELECT count(*) FROM likes l where l."projectName" = p."projectName") as likeCount,'
+                  +'(SELECT count(*) FROM follows f where f."projectName" = p."projectName") as followCount, a."pictureAddress" FROM projects p '
                   +'NATURAL JOIN attaches a '
                   +'WHERE "projectStartDate" <= cast(now() as date) '
                   +'AND "projectDeadline" > cast(now() as date) '
                   +'ORDER BY "projectStartDate", "projectName", "pictureAddress" DESC';
+
 
 router.get('/', function(req, res, next) {
 
@@ -40,6 +43,17 @@ router.get('/', function(req, res, next) {
   }
 
   function getAllProjects() {
+    console.log(project_query);
+    pool.query(project_query, (err, data) => {
+      if(data != null){
+        projTemp = data.rows;
+        console.log(projTemp);
+      }
+      renderPage();
+    });
+  }
+
+  function getLikes() {
     console.log(project_query);
     pool.query(project_query, (err, data) => {
       if(data != null){
